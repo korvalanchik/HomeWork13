0afe83d264aa51b73e2613ea3d497e0999b1a376
korvalanchik <korvalan@yahoo.com>
2023-10-08 22:34:34 +0300

    Start two

--- src/main/java/RelWithAPI.java
+++ src/main/java/RelWithAPI.java
@@ -11,19 +11,45 @@ import java.nio.file.Path;
 
 public class RelWithAPI {
     private static final String TEST_URL =
-            "https://jsonplaceholder.typicode.com/users";
+            "https://petstore.swagger.io/v2/pet";
 
     public static void main(String[] args) throws IOException {
         sendGET();
         sendPOST();
+        sendGETbyId();
+        sendPUT();
+        sendGETbyId();
     }
 
     private static void sendGET() throws IOException {
-        URL url = new URL(TEST_URL);
+        URL url = new URL(TEST_URL+"/findByStatus?status=pending,sold");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
         int responseCode = connection.getResponseCode();
         System.out.println("GET response code: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+    }
+    private static void sendGETbyId() throws IOException {
+        URL url = new URL(TEST_URL+"/332233");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+        int responseCode = connection.getResponseCode();
+        System.out.println("GET by Id response code: " + responseCode);
         if (responseCode == HttpURLConnection.HTTP_OK) {
             BufferedReader in =
                     new BufferedReader(
@@ -44,17 +70,18 @@ public class RelWithAPI {
         URL url = new URL(TEST_URL);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
         connection.setDoOutput(true);
         OutputStream os = connection.getOutputStream();
-        Path f = new File("D:\\JavaProjects\\HomeWork13\\src\\main\\java\\user.json").toPath();
-        System.out.println(f.toString());
-        os.write(Files.readAllBytes(new File("D:\\JavaProjects\\HomeWork13\\src\\main\\java\\user.json").toPath()));
+        Path f = new File("D:\\JavaProjects\\HomeWork13\\src\\main\\java\\pet_new.json").toPath();
+//        System.out.println(f.toString());
+        os.write(Files.readAllBytes(new File("D:\\JavaProjects\\HomeWork13\\src\\main\\java\\pet_new.json").toPath()));
         os.flush();
         os.close();
 
         int responseCode = connection.getResponseCode();
         System.out.println("POST response code: " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_CREATED) {
+        if (responseCode == HttpURLConnection.HTTP_OK) {
             BufferedReader in =
                     new BufferedReader(
                             new InputStreamReader(connection.getInputStream()));
@@ -67,6 +94,36 @@ public class RelWithAPI {
             System.out.println(response.toString());
         } else {
             System.out.println("POST request not worked");
+        }
+    }
+    private static void sendPUT() throws IOException {
+        URL url = new URL(TEST_URL);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("PUT");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+        OutputStream os = connection.getOutputStream();
+        Path f = new File("D:\\JavaProjects\\HomeWork13\\src\\main\\java\\pet_update.json").toPath();
+//        System.out.println(f.toString());
+        os.write(Files.readAllBytes(new File("D:\\JavaProjects\\HomeWork13\\src\\main\\java\\pet_update.json").toPath()));
+        os.flush();
+        os.close();
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("PUT response code: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+        } else {
+            System.out.println("PUT request not worked");
         }
     }
 }
